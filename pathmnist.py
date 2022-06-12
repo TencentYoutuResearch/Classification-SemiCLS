8b0614c001cd7d04dfce8a641868eba69b6285bd
Mert Sayar <mert.sayar@gmail.com>
2022-06-12 19:50:31 +0200

    Update pathmnist.py

--- pathmnist.py
+++ pathmnist.py
@@ -12,7 +12,7 @@ from medmnist import INFO, Evaluator
 from .transforms.randaugment import RandAugmentMC
 
 # split CIFAR10 into labled/unlabed/val set
-def get_pathmnist(args, root):
+def get_pathmnist(args):
     transform_all = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[.5], std=[.5])
